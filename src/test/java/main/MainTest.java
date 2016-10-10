1a82551f1c2b801cd6e251f841447aadacf664e6
jjcomi <jjcomi1989@MBP-di-Giacomo.homenet.telecomitalia.it>
2016-10-11 01:35:23 +0200

    Fixed maven build

--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,12 +1,27 @@
 package main;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import junit.extensions.TestSetup;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.log4j.Logger;
+import java.util.Currency;
+import java.util.Locale;
 
 /**
  * Created by jjcomi1989 on 11/10/16.
  */
 
-public class MainTest {
+public class MainTest extends TestCase {
+    private static Logger logger = Logger.getRootLogger();
+    private static Currency currencyForTest;
 
+    public static TestSetup suite() {
+        TestSetup setup = new TestSetup(new TestSuite(MainTest.class)) {
+            protected void setUp() throws Exception {
+                logger.info("Setup configuration for this test suite");
+                currencyForTest = Currency.getInstance(Locale.US);
+            }
+        };
+        return setup;
+    }
 }
